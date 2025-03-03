0146ef702a129e5dc6459d34f2a821c2fa6bbed8
MaazAli8460 <maazali8460@gmail.com>
2025-03-03 17:36:51 +0500

    Code added to the folders notification services, Booking Services and updated in userservice/index.js

--- UserService/index.js
+++ UserService/index.js
@@ -74,6 +74,16 @@ app.post('/login', async (req, res) => {
 });
 
 // Start the server
-app.listen(5000, () => {
-    console.log('UserService is running on port 5000');
+// app.listen(5000, () => {
+//     console.log('UserService is running on port 5000');
+// });
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Event Booking API!');
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
